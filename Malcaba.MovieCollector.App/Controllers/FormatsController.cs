8fa2ec92aaf46f7a8cfb5da6c35a6fa520d1c95b
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2018-06-05 12:02:29 -0700

    Add auto-generated controllers/views

---
+++ Malcaba.MovieCollector.App/Controllers/FormatsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Malcaba.MovieCollector.Data.Models;
+
+namespace Malcaba.MovieCollector.App.Controllers
+{
+    public class FormatsController : Controller
+    {
+        private readonly MovieDbContext _context;
+
+        public FormatsController(MovieDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Formats
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Format.ToListAsync());
+        }
+
+        // GET: Formats/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var format = await _context.Format
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (format == null)
+            {
+                return NotFound();
+            }
+
+            return View(format);
+        }
+
+        // GET: Formats/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Formats/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Format format)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(format);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(format);
+        }
+
+        // GET: Formats/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var format = await _context.Format.FindAsync(id);
+            if (format == null)
+            {
+                return NotFound();
+            }
+            return View(format);
+        }
+
+        // POST: Formats/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Format format)
+        {
+            if (id != format.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(format);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FormatExists(format.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(format);
+        }
+
+        // GET: Formats/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var format = await _context.Format
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (format == null)
+            {
+                return NotFound();
+            }
+
+            return View(format);
+        }
+
+        // POST: Formats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var format = await _context.Format.FindAsync(id);
+            _context.Format.Remove(format);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FormatExists(int id)
+        {
+            return _context.Format.Any(e => e.Id == id);
+        }
+    }
+}
--- Malcaba.MovieCollector.App/Startup.cs
+++ Malcaba.MovieCollector.App/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Malcaba.MovieCollector.Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,8 +33,11 @@ namespace Malcaba.MovieCollector.App
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            var sqliteConnString = "DataSource=:memory:";
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<MovieDbContext>(options => options.UseInMemoryDatabase("MovieCollector"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
