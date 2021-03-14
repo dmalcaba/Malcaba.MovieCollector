07d20191a0f76d844672f694ec20abdafcb592a5
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2021-03-13 18:11:40 -0800

    Remove unnecessary usings

--- Malcaba.MovieCollector.App/Startup.cs
+++ Malcaba.MovieCollector.App/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using Malcaba.MovieCollector.Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
