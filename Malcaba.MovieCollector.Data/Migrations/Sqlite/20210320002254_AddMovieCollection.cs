39a970712dc745b0ed9c1664a441cb6bcdb788c8
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2021-03-22 17:07:13 -0700

    Update migrations

---
+++ Malcaba.MovieCollector.Data/Migrations/Sqlite/20210320002254_AddMovieCollection.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Malcaba.MovieCollector.Data.Migrations.Sqlite
+{
+    public partial class AddMovieCollection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Collection",
+                table: "Movie",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Dolby",
+                table: "Movie",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "FullScreen",
+                table: "Movie",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Collection",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Dolby",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "FullScreen",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Movie");
+        }
+    }
+}
