3641b323171470a0439fafcbe15e02cd732b72ad
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2021-04-04 13:17:27 -0700

    Updata test project
    Add tests

--- Malcaba.MovieCollector.Tests/MovieJsonExtractor.cs
+++ Malcaba.MovieCollector.Tests/MovieJsonExtractor.cs
@@ -1,73 +1,64 @@
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+using Malcaba.MovieCollector.Data.Services;
 using System;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Malcaba.MovieCollector.Tests
 {
     public class MovieJsonExtractor
     {
-        private const string Url = "";
-        private const int TwoSeconds = 2000;
-
-        [Fact]
-        public async Task ExtractTestAsync()
-        {
-            var options = new ChromeOptions();
-            options.AddArgument("no-sandbox");
-            options.AddArgument("ignore-certificate-errors");
-            options.PageLoadStrategy = PageLoadStrategy.Normal;
-
-            IWebDriver browser = new ChromeDriver(options)
-            {
-                Url = Url
-            };
-
-            var movieList = Tools.ReadFile("MovieList.txt");
-
-            foreach (var title in movieList)
-            {
-                var result = GetJsonResult(browser, title);
-
-                await File.WriteAllTextAsync($@"..\..\..\..\Malcaba.MovieCollector.Data\Json\{title.Replace("'","").Replace(" ","-").ToLower()}.json", result);
-            }
-
-
-            browser.Quit();
-        }
-
         [Fact]
         public void TestRead()
         {
-            var test = Tools.ReadFile("MovieList.txt");
+            var jsonString = @"{
+  ""Title"": ""Baby Driver"",
+  ""Year"": ""2017"",
+  ""Rated"": ""R"",
+  ""Released"": ""28 Jun 2017"",
+  ""Runtime"": ""113 min"",
+  ""Genre"": ""Action, Crime, Drama, Music, Thriller"",
+  ""Director"": ""Edgar Wright"",
+  ""Writer"": ""Edgar Wright"",
+  ""Actors"": ""Ansel Elgort, Jon Bernthal, Jon Hamm, Eiza González"",
+  ""Plot"": ""After being coerced into working for a crime boss, a young getaway driver finds himself taking part in a heist doomed to fail."",
+  ""Language"": ""English, American Sign Language"",
+  ""Country"": ""UK, USA"",
+  ""Awards"": ""Nominated for 3 Oscars. Another 42 wins & 62 nominations."",
+  ""Poster"": ""https://m.media-amazon.com/images/M/MV5BMjM3MjQ1MzkxNl5BMl5BanBnXkFtZTgwODk1ODgyMjI@._V1_SX300.jpg"",
+  ""Ratings"": [
+    {
+      ""Source"": ""Internet Movie Database"",
+      ""Value"": ""7.6/10""
+    },
+    {
+      ""Source"": ""Rotten Tomatoes"",
+      ""Value"": ""92%""
+    },
+    {
+      ""Source"": ""Metacritic"",
+      ""Value"": ""86/100""
+    }
+  ],
+  ""Metascore"": ""86"",
+  ""imdbRating"": ""7.6"",
+  ""imdbVotes"": ""445,001"",
+  ""imdbID"": ""tt3890160"",
+  ""Type"": ""movie"",
+  ""DVD"": ""10 Jul 2017"",
+  ""BoxOffice"": ""$107,825,862"",
+  ""Production"": ""Working Title Films, Big Talk Pictures"",
+  ""Website"": ""N/A"",
+  ""Response"": ""True""
+}";
 
-            Assert.True(test.Count > 0);
+            var movieService = new MovieService(null);
 
-        }
+            var movie = movieService.FileToObject(jsonString);
 
-        private string GetJsonResult(IWebDriver browser, string title)
-        {
-            var titleInput = browser.FindElement(By.Id("t"));
-            titleInput.Click();
-            titleInput.Clear();
-            titleInput.SendKeys(title);
+            Assert.NotNull(movie);
+            Assert.Equal("Baby Driver", movie.Title);
+            Assert.Equal(new DateTime(2017, 06, 28), movie.Released);
+            Assert.Equal(113, movie.RuntimeInMinutes);
 
-            var searchButton = browser.FindElement(By.Id("search-by-title-button"));
-            searchButton.Click();
-
-            // Wait for API to finish
-            Thread.Sleep(TwoSeconds);
-
-            var result = browser.FindElement(By.CssSelector("#search-by-title-response > .alert"));
-
-            return result.Text;
-        }
-
-        private void SaveJsonToFile(string json)
-        { 
         }
     }
 }
---
+++ Malcaba.MovieCollector.Tests/MovieServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Malcaba.MovieCollector.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Malcaba.MovieCollector.Tests
+{
+    public class MovieServiceTests
+    {
+
+        [Theory]
+        [InlineData("The Terminator", "Terminator, The")]
+        [InlineData("A Terminator", "Terminator, A")]
+        [InlineData("Terminator", "Terminator")]
+
+        public void DetermineSortTitleTests(string title, string expected)
+        {
+            var movieService = new MovieService(null);
+
+            var result = movieService.DetermineSortTitle(title);
+
+            Assert.Equal(expected, result);
+        
+        }
+    }
+}
