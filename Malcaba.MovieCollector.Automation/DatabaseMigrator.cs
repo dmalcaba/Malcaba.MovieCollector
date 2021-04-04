8eb4e7f4f39f08f271eef663cd48bd4a836770f4
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2021-04-04 13:15:43 -0700

    Add database migrator
    This will generate a database with data based on json

---
+++ Malcaba.MovieCollector.Automation/DatabaseMigrator.cs
@@ -0,0 +1,32 @@
+﻿using Malcaba.MovieCollector.Data.Models;
+using Malcaba.MovieCollector.Data.Services;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Malcaba.MovieCollector.Automation
+{
+    public static class DatabaseMigrator
+    {
+        public static async Task RunUpdate()
+        {
+            var options = new DbContextOptionsBuilder<MovieDbContext>()
+                .UseSqlite(ConnectionString)
+                .Options;
+
+            using var context = new MovieDbContext(options);
+            await context.Database.EnsureDeletedAsync();
+
+            await context.Database.MigrateAsync();
+
+            var movieService = new MovieService(context);
+
+            await movieService.GetFilesAsync();
+        }
+
+        private static string ConnectionString => new SqliteConnectionStringBuilder
+        {
+            DataSource = "Movies.db"
+        }.ToString();
+    }
+}
